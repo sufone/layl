60e358b4f758a9122e3e1c9e0cae6479c955c670
Naved Islam <navedcoded@gmail.com>
2020-06-14 14:56:27 +0300

    draft 1

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<div>
+<p>Made with 💗 by Naved</p>
+
+</div>
+
+<style>
+p {
+  text-align: center;
+}
+div {
+  position: absolute;
+  bottom: 5px;
+  width: 80vw;
+  text-align: center;
+}
+</style>
--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -1,1 +1,55 @@
-<p>Placeholder explanation</p>
+
+<div>
+<blockquote>
+“So perform the regular prayers in the period from the time the sun is past its zenith till the darkness of the night, and [recite] the Qur’an at dawn— dawn recitation is always witnessed— and during the night wake up and pray, as an extra offering of your own, so that your Lord may raise you to a [highly] praised status.”
+(Surah al-Isra 17:78-79)
+</blockquote>
+
+<p>Divine guidance calls us to sleep parts of the night, and to wake up for worship during other. Worship includes prayer, remembrance, Quran recitation, and more. Knowing the parts of the night also lets us follow the lifestyles of the prophets, may peace be upon them all.
+</p>
+
+<p>
+ Beyond voluntary worship, these parts of the night are important for duties like praying Isha on time. <a href=""> Learn more about these blessed times. </a>
+</p>
+
+<hr>
+
+<p>
+The Islamic night starts at Maghrib, right after sunset, and lasts until Fajr, which is dawn.
+</p>
+<p>
+The time of Maghrib or Fajr depends on your location. The calculation of Maghrib time is clear and simple in most places. But the calculation of Fajr relies upon different methods. The best method is different in each place, and can change with the seasons too. This app uses a well-tested calculation method, but it may be inaccurate.
+</p>
+
+<p>
+
+So, please <strong>double-check the Maghrib and Fajr times</strong> given by this app—if they are correct then the other times will be correct. Also, safeguard your worship by keeping enough time before or after the times given by any app.
+</p>
+
+<p>
+
+These warnings aren't pretty, but they're part of responsible app development. May Allah accept our worship, <em>ameen</em>.
+</p>
+</div>
+
+<Footer />
+
+<script>
+import Footer from './Footer.svelte'
+</script>
+
+<style>
+p, blockquote {
+  margin: 10px;
+  }
+div {
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+blockquote {
+      border-left: 3px solid #67b6ff;
+      padding-left: 20px;
+      font-style: italic;
+}
+
+</style>
