fc1af18ab7be4759980b3d4225af396574f76858
Naved Islam <navedcoded@gmail.com>
2020-06-18 11:06:17 +0300

    imrpoved checker, thinner border

--- src/components/TimesTable.svelte
+++ src/components/TimesTable.svelte
@@ -35,11 +35,11 @@
 
 <ul>
   <li id="time-0">Maghrib: night start<strong>{prayerTimes[0]}</strong></li>
-  <li id="time-1">First-sixth ends <strong>{prayerTimes[1]}</strong> </li>
+  <li id="time-1" class="dark-bg">First-sixth ends <strong>{prayerTimes[1]}</strong> </li>
   <li id="time-2">First-third ends <strong>{prayerTimes[2]}</strong></li>
-  <li id="time-3">Half the night <strong>{prayerTimes[3]}</strong></li>
+  <li id="time-3" class="dark-bg">Half the night <strong>{prayerTimes[3]}</strong></li>
   <li id="time-4">Last-third starts<strong>{prayerTimes[4]}</strong></li>
-  <li id="time-5">Last-sixth starts <strong>{prayerTimes[5]}</strong></li>
+  <li id="time-5" class="dark-bg">Last-sixth starts <strong>{prayerTimes[5]}</strong></li>
   <li id="time-6">Fajr: night end<strong>{prayerTimes[6]}</strong></li>
 </ul>
 
@@ -81,45 +81,50 @@ li:before {
 	content: "1⁄₆";
 }
 #time-1 {
-  background-color: rgba(0, 0, 0, 0.096);
-}
-#time-1 {
-    border-left: 2px solid #80a9e6;
+    border-left: 1.5px solid #80a9e6;
 }
 #time-2:before {
   content: "2⁄6";
 }
 #time-2 {
-    border-left: 2px solid #9a9ccc;
+    border-left: 1.5px solid #9a9ccc;
 
 }
 #time-3:before {
   content: "3⁄6";
 }
 #time-3 {
-    background-color: rgba(0, 0, 0, 0.096);
-    border-left: 2px solid #b38eb3;
+    border-left: 1.5px solid #b38eb3;
 
 }
 #time-4:before {
   content: "4⁄6";
 }
 #time-4 {
-      border-left: 2px solid #cc819a;
+      border-left: 1.5px solid #cc819a;
 
 }
 #time-5:before {
   content: "5⁄6";
 }
 #time-5 {
-    background-color: rgba(0, 0, 0, 0.096);
-    border-left: 2px solid #e67480;
+    border-left: 1.5px solid #e67480;
 }
 #time-6:before {
   content: url("/assets/sun.svg");
   width: 24px;
   height: 24px;
   padding-top: 6px;
+}
+.dark-bg {
+  background-color: rgba(0, 0, 0, 0.04);
+
+}
+
+@media (prefers-color-scheme: dark) {
+  .dark-bg {
+    background-color: rgba(255, 255, 255, 0.048);
+  }
 }
 
 @media only screen and (max-width: 270px) {
