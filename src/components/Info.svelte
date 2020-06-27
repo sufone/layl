b73a06f682981638ea5e5c186468901676cdca93
Naved Islam <navedcoded@gmail.com>
2020-06-28 01:18:41 +0300

    shorter text

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -1,23 +1,20 @@
 
 <div id="info">
 <blockquote>
-“And during the night wake up and pray, as an extra offering of your own, so that your Lord may raise you to a [highly] praised status.”
-(Surah al-Isra 17:79)
+“And during the night wake up and pray، as an extra offering of your own، so that your Lord may raise you to a [highly] praised status.” [Surah al-Isra 79]
 </blockquote>
 
 <a href=""> Learn more about these blessed times and the importance of the night. </a>
 
-<p>
-The Islamic night starts at Maghrib and lasts until Fajr. This app uses official calculation methods from around the world, but it may be inaccurate for some locations.
+<p>Layl is a quick and simple app that tells you the divisions of the night at your location, allowing you to easily plan worship and make the most of your night.
 </p>
 
 <p>
 
-So, please <strong>double-check the Maghrib and Fajr times</strong> given by this app—if they are correct then the other times will also be correct. Safeguard your worship by keeping enough time before or after the times given by any app.
+Please <strong>double-check the Maghrib and Fajr times</strong> given by this app—if they are correct then the other times will also be correct. 
 </p>
-<p>
 
-These warnings aren't pretty, but they're part of responsible app development. May Allah accept our worship, <em>ameen</em>.
+<p> May Allah accept our worship, <em>ameen</em>.
 </p>
 
 <div id="subscribe-form">
