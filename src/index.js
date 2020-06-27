6884b2eb6e40b38cdeda5ec792571e33801f4816
Naved Islam <navedcoded@gmail.com>
2020-06-28 01:03:28 +0300

    force immediate update
    aggressive is better in case need to push an emergency update!

--- src/index.js
+++ src/index.js
@@ -7,7 +7,28 @@ window.app = new App({
 });
 
 if (process.env.NODE_ENV === 'production') {
+	if ('serviceWorker' in navigator) {
 
-	// Service Worker registration
-	require('offline-plugin/runtime').install();
+		const runtime = require('offline-plugin/runtime');
+
+		runtime.install({
+			onUpdating: () => {
+				console.log('SW Event:', 'onUpdating');
+			},
+			onUpdateReady: () => {
+				console.log('SW Event:', 'onUpdateReady');
+				// Tells to new SW to take control immediately
+				runtime.applyUpdate();
+			},
+			onUpdated: () => {
+				console.log('SW Event:', 'onUpdated');
+				// Reload the webpage to load into the new version
+				window.location.reload();
+			},
+		
+			onUpdateFailed: () => {
+				console.log('SW Event:', 'onUpdateFailed');
+			}
+		});
+	}
 }
