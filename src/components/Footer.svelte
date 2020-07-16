56b8545e4214cecd63b9c9022fb645a5551f5075
Naved Islam <navedcoded@gmail.com>
2020-07-16 11:30:00 +0300

    gh button

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,18 +1,21 @@
 
 <p>Beta v2.13 · Made with 💗 by <a href="https://navedislam.com/apps">Naved</a></p>
 
+<div>
+<a class="github-button" href="https://github.com/mr-islam/layl-pwa" data-color-scheme="no-preference: light; light: light; dark: dark;" data-show-count="true" aria-label="Star mr-islam/layl-pwa on GitHub">Star</a>
+</div>
 
 <style>
-
+a {
+  text-align: center;
+}
 p {
 margin-top: 6em;
   text-align: center;
   font-size: 0.8em;
 }
 div {
-  position: absolute;
-  bottom: 5px;
-
+  margin-top: 0.5em;
   text-align: center;
 }
 
