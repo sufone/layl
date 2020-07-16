ccc66daec77e69d216406632a55ce422a17691f5
Naved Islam <navedcoded@gmail.com>
2020-07-16 12:15:12 +0300

    githuib class for button

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,10 @@
 
 <p>Beta v2.13 · Made with 💗 by <a href="https://navedislam.com/apps">Naved</a></p>
 
-<div>
+<div class="github">
 <a class="github-button" href="https://github.com/mr-islam/layl-pwa" data-color-scheme="no-preference: light; light: light; dark: dark;" data-show-count="true" aria-label="Star mr-islam/layl-pwa on GitHub">Star</a>
 </div>
+
 
 <style>
 a {
@@ -14,7 +15,7 @@ margin-top: 6em;
   text-align: center;
   font-size: 0.8em;
 }
-div {
+div.github {
   margin-top: 0.5em;
   text-align: center;
 }
