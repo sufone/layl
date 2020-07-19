7c419acb6420453059cfe2fcd7e7277b03e37f15
Naved Islam <navedcoded@gmail.com>
2020-07-19 11:32:46 +0300

    sep compo

---
+++ src/components/Loader.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { SyncLoader } from 'svelte-loading-spinners'
+
+  export let color
+
+
+</script>
+
+<div style="text-align: center; ">
+	<div style="display: inline-block; padding: 10px;">
+    	<SyncLoader size="30" color={color} unit="px"></SyncLoader> 
+	</div>
+</div>
+
+
+<style>
+
+</style>
