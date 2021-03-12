5f76f79034a7e89014d741055ca2cd06350fa135
Naved Islam <navedcoded@gmail.com>
2021-03-12 13:08:38 +0200

    comment about risk of locale name in language picker for english

--- src/components/LanguagePicker.svelte
+++ src/components/LanguagePicker.svelte
@@ -12,7 +12,8 @@
 <div class="select">
     <select id="language-picker" bind:value={$locale}>
         <option value="ar">🇪🇬 العربية</option>
-        <option value="en-US">🇺🇸 English </option>
+        <option value="en-US">🇺🇸 English </option> 
+        <!-- calling it en-US might be an issue, but I think it falls back to our proivded en locale so ok -->
     </select>
 </div>
 
