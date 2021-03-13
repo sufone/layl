2471309b691659bb45141f47b376104dadad0b5e
Naved Islam <navedcoded@gmail.com>
2021-03-13 13:09:07 +0200

    remember and load last used locale

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,12 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
+import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 
 register('en', () => import('../locales/en.json'));
 register('ar', () => import('../locales/ar.json'));
+
+locale.subscribe((value) => {
+	if (value == null) return;
+    localStorage.setItem('locale', value)
+});
 
 export function startClient() {
     init({
@@ -9,6 +14,6 @@ export function startClient() {
         loadingDelay: 200,
         formats: {},
         warnOnMissingMessages: true,
-        initialLocale: getLocaleFromNavigator()
+        initialLocale: localStorage.getItem('locale') || getLocaleFromNavigator()
     });
 }
