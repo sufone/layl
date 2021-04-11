45508e769888f9a3b4215038d574b4d51bf5c044
Naved Islam <navedcoded@gmail.com>
2021-04-11 09:25:14 +0300

    fix #58, hopefully, with forced refresh

--- src/i18n.js
+++ src/i18n.js
@@ -5,13 +5,20 @@ register('ar', () => import('../locales/ar.json'));
 
 locale.subscribe((value) => {
 	if (value == null) return;
-    localStorage.setItem('locale', value)
+
+    // Handle manual user locale change by forcing refresh, and prevent GH issue #58. If locale 
+    // value in store !== current localStorage value, update the latter and refresh with the new locale.
+    // Terrible… but we need to release somehow - Naved April 11, 2021 
+    if (value !== localStorage.getItem('locale')) {
+        localStorage.setItem('locale', value)
+        location.reload(); 
+    }
 });
 
 export function startClient() {
     init({
         fallbackLocale: 'en',
-        loadingDelay: 0,
+        loadingDelay: 200,
         formats: {},
         warnOnMissingMessages: true,
         initialLocale: localStorage.getItem('locale') || getLocaleFromNavigator()
