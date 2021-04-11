029ea604fe957efeb1e76de810683da94fd23ee5
Naved Islam <navedcoded@gmail.com>
2021-04-11 09:42:42 +0300

    move refresh and localstoerage handler to languagepiocker comp

--- src/i18n.js
+++ src/i18n.js
@@ -3,18 +3,6 @@ import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 register('en', () => import('../locales/en.json'));
 register('ar', () => import('../locales/ar.json'));
 
-locale.subscribe((value) => {
-	if (value == null) return;
-
-    // Handle manual user locale change by forcing refresh, and prevent GH issue #58. If locale 
-    // value in store !== current localStorage value, update the latter and refresh with the new locale.
-    // Terrible… but we need to release somehow - Naved April 11, 2021 
-    if (value !== localStorage.getItem('locale')) {
-        localStorage.setItem('locale', value)
-        location.reload(); 
-    }
-});
-
 export function startClient() {
     init({
         fallbackLocale: 'en',
