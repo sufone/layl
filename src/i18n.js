b31a74cf309c0396e28920b5414689268d37034e
Naved Islam <navedcoded@gmail.com>
2021-04-12 12:39:27 +0300

    revert force refresh to test maybe the loading delay is enough

--- src/i18n.js
+++ src/i18n.js
@@ -5,14 +5,7 @@ register('ar', () => import('../locales/ar.json'));
 
 locale.subscribe((value) => {
 	if (value == null) return;
-
-    // Handle manual user locale change by forcing refresh, and prevent GH issue #58. If locale 
-    // value in store !== current localStorage value, update the latter and refresh with the new locale.
-    // Terrible… but we need to release somehow - Naved April 11, 2021 
-    if (value !== localStorage.getItem('locale')) {
-        localStorage.setItem('locale', value)
-        location.reload(); 
-    }
+    localStorage.setItem('locale', value)
 });
 
 export function startClient() {
