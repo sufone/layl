d5248a25321432984a3dd8c3b638278e4c4e62aa
mhdtarekalkasemi@gmail.com <mhdtarekalkasemi@gmail.com>
2023-02-07 21:23:15 +0100

    60 percent of sv translations done

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 
 register('en', () => import('../locales/en.json'));
 register('ar', () => import('../locales/ar.json'));
+register('sv', () => import('../locales/sv.json'))
 
 locale.subscribe((value) => {
 	if (value == null) return;
