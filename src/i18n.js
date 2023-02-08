75c5e31da0d1d45d69d68db5ed6b3083d0b4722b
mhdtarekalkasemi@gmail.com <mhdtarekalkasemi@gmail.com>
2023-02-08 21:29:10 +0100

    maybe this ?

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,7 @@ import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 
 register('en', () => import('../locales/en.json'));
 register('ar', () => import('../locales/ar.json'));
-register('sv', () => import('../locales/sv.json'))
+register('sv', () => import('../locales/sv.json'));
 
 locale.subscribe((value) => {
 	if (value == null) return;
