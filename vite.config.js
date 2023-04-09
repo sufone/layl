ec48bf6f96161b088f2b93e6e71982e33f82a2a7
Tarek <mhdtarekalkasemi@gmail.com>
2023-04-09 21:04:05 +0200

    Favicon & PWA done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+    includeAssets: ['/assets/icon.png'],
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'Layl',
+      short_name: 'Layl',
+      description: ' 🌙 Calculator for tahajjud, qiyam, and worship & sleep in an Islamic night!',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/assets/icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+      ]
+    }
+  })
+]
 })
